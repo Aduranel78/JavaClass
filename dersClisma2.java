42acc44b57a703e3e72f9b5efaae4da5d0e5892c
Aduranel78 <56616738+Aduranel78@users.noreply.github.com>
2019-10-24 13:17:22 -0400

    commit2

--- dersClisma2.java
+++ dersClisma2.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class dersClisma2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("provide a temperature");
+        System.out.println("provide a temperature is");
         int temperature = scan.nextInt();
         if (temperature > 80) {
             System.out.println("HOT");
